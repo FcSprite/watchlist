d8a5d82328721116ba3f99dd4acffda474c181ba
fcsprite <fcsprite@163.com>
2020-11-19 21:20:17 +0800

    添加主页

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 
 app = Flask(__name__)
 
@@ -18,13 +18,29 @@ app = Flask(__name__)
 #     print(url_for('user_page',name='fcsprite'))
 #     return 'Test page'
 #
-
+@app.route('/')
+def index():
+    return  render_template('index.html',name=name,movies=movies)
 
 @app.route('/home')
-def index():
+def home():
     return 'Hello,FcSprite!' \
            '你好,幻彩雪碧!'\
             '<h1>Hello Flask!</h1><img src="http://helloflask.com/totoro.gif">'
+name = '幻彩雪碧'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+        ]
+
 if __name__ == '__main__':
     # print(app.url_map)
     app.run()
